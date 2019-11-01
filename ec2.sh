e9f209ecfd9d47348a949bd4f1f616eb236ecad3
Bruno Arthur <brunoartc@yahoo.com>
2019-11-01 02:53:38 -0300

    add start app in tmux session

--- ec2.sh
+++ ec2.sh
@@ -8,4 +8,4 @@ sudo apt-get update -y
 sudo apt-get install python-pip -y
 sudo apt install python3-pip -y
 pip3 install -r requirments.txt
-python3 main.py
+tmux -L todo new-session -d -s myapp 'python3 main.py' 
