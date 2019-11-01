dba9bb6ee64e7b0cf1dbf07da718fc84d9ded6b2
Bruno Arthur <brunoartc@yahoo.com>
2019-11-01 02:37:00 -0300

    update script

--- ec2.sh
+++ ec2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-cd ~
+cd /home/ubuntu
 git clone https://github.com/brunoartc/cloud_serverless
 cd cloud_serverless
+sudo apt-get install software-properties-common
+sudo apt-add-repository universe
+sudo apt-get update -y
+sudo apt-get install python-pip -y
+sudo apt install python3-pip -y
 pip3 install -r requirments.txt
 python3 main.py
